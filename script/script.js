f7f8ecbfb5f8efb580adbb838d37926d48b87183
Thimen-Labeeuw <Thimen.Labeeuw@student.tsaam.be>
2025-03-26 10:55:23 +0100

    v1

--- script/script.js
+++ script/script.js
@@ -18,6 +18,54 @@ const users = [
     email: "piet.pollekens@gmail.com",
     password: "123Test",
   },
+  {
+    fullname: "PietPollekens",
+    email: "piet.pollekens@gmail.com",
+    password: "123Test",
+  },
 ];
 
 // hieronder komt jullie code - veel succes
+
+//HTML
+const LijstNamen = document.querySelector("#lijstNamen");
+
+//local variable
+
+//namen wissen
+LijstNamen.innerHTML = "";
+updateLijst();
+
+//namen toeveogen in lijst
+function updateLijst() {
+  users.forEach((user) => {
+    const listItem = document.createElement("li");
+    const username = document.createElement("span");
+    const email = document.createElement("span");
+    username.className = "username";
+    email.className = "email";
+    username.innerHTML = user.fullname;
+    email.innerHTML = user.email;
+    if (checkDubbelUser(user.fullname) == true) {
+      listItem.className = "dubbelUser";
+    }
+    listItem.appendChild(username);
+    listItem.appendChild(email);
+    LijstNamen.appendChild(listItem);
+  });
+}
+
+//dubbeluser
+function checkDubbelUser(userName) {
+  let IsDubbel = false;
+  users.forEach((user) => {
+    if (user.fullname == userName) {
+      IsDubbel = true;
+    } else {
+      IsDubbel = false;
+    }
+  });
+  return IsDubbel;
+}
+
+//registreren van nieuwe gebruiker
