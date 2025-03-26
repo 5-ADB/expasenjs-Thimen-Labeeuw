0f64c0b0f15111af5d26cd4fc77e4ea20884c77f
Thimen-Labeeuw <Thimen.Labeeuw@student.tsaam.be>
2025-03-26 11:43:25 +0100

    v2

--- script/script.js
+++ script/script.js
@@ -38,6 +38,7 @@ updateLijst();
 
 //namen toeveogen in lijst
 function updateLijst() {
+  LijstNamen.innerHTML = "";
   users.forEach((user) => {
     const listItem = document.createElement("li");
     const username = document.createElement("span");
@@ -48,6 +49,8 @@ function updateLijst() {
     email.innerHTML = user.email;
     if (checkDubbelUser(user.fullname) == true) {
       listItem.className = "dubbelUser";
+    } else {
+      listItem.className = "";
     }
     listItem.appendChild(username);
     listItem.appendChild(email);
@@ -57,15 +60,51 @@ function updateLijst() {
 
 //dubbeluser
 function checkDubbelUser(userName) {
-  let IsDubbel = false;
-  users.forEach((user) => {
-    if (user.fullname == userName) {
-      IsDubbel = true;
-    } else {
-      IsDubbel = false;
+  let i = 0;
+  users.forEach((CurrentUser) => {
+    if (CurrentUser.fullname == userName) {
+      i++;
     }
   });
-  return IsDubbel;
+  console.log(i);
 }
 
 //registreren van nieuwe gebruiker
+const RegisterButton = document.querySelector("#registreer");
+const warningText = document.querySelector("#warningText");
+const InputFieldName = document.querySelector("#name");
+const InputFieldEmail = document.querySelector("#email");
+const InputFieldPassword = document.querySelector("#password");
+
+RegisterButton.addEventListener("click", addUser);
+
+function addUser() {
+  if (
+    InputFieldName.value == "" ||
+    InputFieldEmail.value == "" ||
+    InputFieldPassword.value == ""
+  ) {
+    warningText.innerHTML = "een van de velden is niet ingevuld";
+  } else {
+    users.push({
+      fullname: InputFieldName.value,
+      email: InputFieldEmail.value,
+      password: InputFieldPassword.value,
+    });
+    updateLijst();
+  }
+}
+
+//filter
+const filterBtn = document.querySelector("#filterBtn");
+const InputFieldFilter = document.querySelector("#filter");
+const FilterWarning = document.querySelector("#FilterWarning");
+
+filterBtn.addEventListener("click", FilterName);
+
+function FilterName() {
+  if (InputFieldFilter.value.length < 4) {
+    FilterWarning.innerHTML = "filter incorrect";
+  } else {
+  }
+}
